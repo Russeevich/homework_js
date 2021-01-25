7320619f218e70c1a7b89d5a01b0792d4d859fd5
Russeevich <kokanazar@yandex.ru>
2021-01-25 18:59:46 +0300

    homework_2

---
+++ projects/array-objects/index.js
@@ -0,0 +1,91 @@
+/* ДЗ 2 - работа с массивами и объектами */
+
+/*
+ Задание 1:
+
+ Напишите аналог встроенного метода forEach для работы с массивами
+ Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   forEach([1, 2, 3], (el) => console.log(el))
+ */
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
+
+/*
+ Задание 2:
+
+ Напишите аналог встроенного метода map для работы с массивами
+ Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
+ */
+function map(array, fn) {
+  const temp = [];
+  for (let i = 0; i < array.length; i++) {
+    temp.push(fn(array[i], i, array));
+  }
+  return temp;
+}
+
+/*
+ Задание 3:
+
+ Напишите аналог встроенного метода reduce для работы с массивами
+ Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
+
+ Пример:
+   reduce([1, 2, 3], (all, current) => all + current) // 6
+ */
+function reduce(array, fn, initial) {
+  let sum = initial ? initial : array[0];
+  for (let i = initial ? 0 : 1; i < array.length; i++) {
+    sum = fn(sum, array[i], i, array);
+  }
+  return sum;
+}
+
+/*
+ Задание 4:
+
+ Функция должна перебрать все свойства объекта, преобразовать их имена в верхний регистр и вернуть в виде массива
+
+ Пример:
+   upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
+ */
+function upperProps(obj) {
+  const temp = [];
+  for (const field in obj) {
+    temp.push(field.toUpperCase());
+  }
+  return temp;
+}
+
+/*
+ Задание 5 *:
+
+ Функция принимает объект и должна вернуть Proxy для этого объекта
+ Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
+
+ Пример:
+   const obj = createProxy({});
+   obj.foo = 2;
+   console.log(obj.foo); // 4
+ */
+function createProxy(obj) {
+  const valid = {
+    set: function (object, props, value) {
+      object[props] = value ** 2;
+      return true;
+    },
+  };
+  const proxy = new Proxy(obj, valid);
+
+  return proxy;
+}
+
+export { forEach, map, reduce, upperProps, createProxy };
