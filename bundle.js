4b84dd32804aeec4dd1ff6aa551312cd2a3e4dee
Russeevich <kokanazar@yandex.ru>
2021-02-12 21:29:05 +0300

    update

--- bundle.js
+++ bundle.js
@@ -4,12 +4,10 @@ let index = 0
 
 function init() {
 
-    let points = {
-        "type": "FeatureCollection",
-        features: [
+    let points = [
 
-        ]
-    }
+    ]
+
 
     document.addEventListener('submit', e => {
         e.preventDefault()
@@ -26,45 +24,40 @@ function init() {
     })
 
     const UpdateInfo = (user) => {
-        for (let item of points.features) {
-            if (user.formId === item.id.toString()) {
-                let oldHeader = item.properties.balloonContentHeader,
+        for (let item of points) {
+            if (user.formId === item.properties._data.placemarkId.toString()) {
+
+                let oldHeader = item.properties._data.balloonContentHeader,
                     newHeader = `<p><strong>${user.login}:</strong> ${user.place}</p><p>${user.subject}</p>` + oldHeader
-                item.properties.balloonContentHeader = newHeader
+                item.properties._data.balloonContentHeader = item.properties._data.clusterCaption = newHeader
 
-                if (item.properties.iconContent >= 1) {
-                    item.properties.iconContent++;
-                    item.options.preset = 'islands#blueCircleIcon'
-                    objectManager.objects.setObjectOptions(item.id, {
-                        preset: item.options.preset
-                    });
-                    // points.features.push({...item, id: Date.now() })
-                } else item.properties.iconContent = 1
+                if (item.properties._data.iconContent >= 1) {
+                    item.properties._data.iconContent++;
+                    UpdateCoords(item.geometry._coordinates)
+                } else item.properties._data.iconContent = 1
 
-                objectManager.objects.balloon.close(item.id)
+                item.balloon.close()
             }
         }
     }
 
+    var customBalloonContentLayout = ymaps.templateLayoutFactory.createClass([
+        '<div class=list>',
+        `<div>{{ properties.geoObjects[0].properties.balloonContentHeader|raw }}<form id='{{properties.geoObjects[0].properties.placemarkId}}'><p><input required placeholder='Введите ваше имя' name='login'></p><p><input name='place' required placeholder='Укажите место'></p><p><textarea name='subject' required placeholder='Оставьте отзыв'></textarea></p><p><input type='submit' value='Добавить'></p></div>`,
+        '</div>'
+    ].join(''));
+
     var myMap = new ymaps.Map('map', {
-            center: [55.76, 37.64],
-            zoom: 10
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
-        objectManager = new ymaps.ObjectManager({
-            clusterize: true,
-            gridSize: 32,
-            clusterDisableClickZoom: true,
-            clusterHideIconOnBalloonOpen: true,
-            geoObjectHideIconOnBalloonOpen: true
-        });
+        center: [55.76, 37.64],
+        zoom: 10
+    }, {
+        searchControlProvider: 'yandex#search'
+    })
     clusterer = new ymaps.Clusterer({
-        preset: 'islands#invertedVioletClusterIcons',
-        groupByCoordinates: false,
         clusterDisableClickZoom: true,
-        clusterHideIconOnBalloonOpen: false,
-        geoObjectHideIconOnBalloonOpen: false
+        groupByCoordinates: true,
+        // clusterBalloonLeftColumnWidth: 0
+        clusterBalloonContentLayout: customBalloonContentLayout
     })
 
     myMap.events.add('click', function(e) {
@@ -73,7 +66,6 @@ function init() {
     })
 
     const deleteAll = (id) => {
-        // points.features = points.features.filter(item => item.id !== id && item.properties.iconContent === '')
         points.features.forEach(item => {
             if (item.id !== id && item.properties.iconContent === '') {
                 points.features = points.features.filter(obj => obj === item)
@@ -83,62 +75,69 @@ function init() {
 
     const UpdateCoords = (coords) => {
         const id = Date.now(),
-            obj = {
-                type: "Feature",
-                id,
-                geometry: {
-                    type: "Point",
-                    coordinates: coords
-                },
-                properties: {
-                    balloonContentBody: `<form id='${id}'><p><input required placeholder='Введите ваше имя' name='login'></p><p><input name='place' required placeholder='Укажите место'></p><p><textarea name='subject' required placeholder='Оставьте отзыв'></textarea></p><p><input type='submit' value='Добавить'></p>`,
-                    balloonContentFooter: `</form>`,
-                    balloonContentHeader: '<strong>Отзыв:</strong>',
-                    // clusterCaption: "<strong><s>Еще</s> одна</strong> метка",
-                    // hintContent: "<strong>Текст  <s>подсказки</s></strong>",
-                    iconContent: ''
-                },
-                options: {
-                    preset: "islands#blueIcon"
-                }
-            }
-        points.features.push(obj)
-        deleteAll(id)
+            obj = new ymaps.Placemark(coords, {
+                placemarkId: id,
+                balloonContentBody: `<form id='${id}'><p><input required placeholder='Введите ваше имя' name='login'></p><p><input name='place' required placeholder='Укажите место'></p><p><textarea name='subject' required placeholder='Оставьте отзыв'></textarea></p><p><input type='submit' value='Добавить'></p>`,
+                balloonContentFooter: `</form>`,
+                balloonContentHeader: '<strong>Отзыв:</strong>',
+                clusterCaption: "<strong>Отзыв:</strong>",
+                hintContent: "<strong>Текст  <s>подсказки</s></strong>",
+                iconContent: ''
+            })
+        points.push(obj)
 
+        console.log(clusterer.getGeoObjects())
 
-        updateMap(id)
+        updateMap(obj)
     }
 
-    const updateMap = (id) => {
-        for (let item of points.features) {
-            if (id === item.id) {
-                objectManager.add(item)
-                objectManager.objects.balloon.open(id)
+    const updateMap = (obj) => {
+        for (let item of points) {
+            if (obj === item) {
+                clusterer.add(obj)
+                myMap.geoObjects.add(clusterer)
             }
         }
+        obj.balloon.open()
     }
 
+    clusterer.events.add('balloonclose', function(event) {
+        const id = event.get('target').properties._data.placemarkId,
+            obj = points.filter(item => item.properties._data.placemarkId === id)
 
-    objectManager.objects.events.add('balloonopen', function(e) {
-
-    });
-
-    objectManager.objects.events.add('balloonclose', function(e) {
-        const objectId = e.get('objectId'),
-            obj = points.features.filter(item => item.id === objectId)
-
-        obj.forEach(item => {
-            if (item.properties.iconContent < 1) {
-                points.features = points.features.filter(item => item.id !== objectId)
-                objectManager.remove([objectId])
-            }
-        })
+        if (obj[0].properties._data.iconContent !== '')
+            return
+        points = points.filter(item => item.properties._data.placemarkId !== id)
+        clusterer.remove(obj)
 
     })
 
+
+    // objectManager.objects.events.add('balloonopen', function(e) {
+    //     console.log(points)
+    // });
+
+    // objectManager.objects.events.add('balloonclose', function(e) {
+    //     const objectId = e.get('objectId'),
+    //         obj = points.features.filter(item => item.id === objectId)
+
+    //     obj.forEach(item => {
+    //         if (item.properties.iconContent < 1) {
+    //             points.features = points.features.filter(item => item.id !== objectId)
+    //             objectManager.remove([objectId])
+    //         }
+    //     })
+
+    // })
+
     // objectManager.objects.options.set('preset', 'islands#blueIcon');
-    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
-    myMap.geoObjects.add(objectManager);
-    myMap.geoObjects.add(clusterer);
+    // objectManager.clusters.options.set({
+    //     'preset': 'islands#blueClusterIcons',
+    //     gridSize: 100,
+    //     minClusterSize: 1,
+    //     synchAdd: true
+    // });
+    // myMap.geoObjects.add(objectManager);
+    myMap.geoObjects.add(clusterer)
 
 }
